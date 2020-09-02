fba8451da48290c975a02b825ade955e80e02625
zyj-notebook <1072431407@qq.com>
2020-09-02 17:14:02 +0800

    listview

---
+++ mylibrary/src/main/java/com/zyj/library/view/pulltorefresh/PullToRefreshRecyclerView.java
@@ -0,0 +1,196 @@
+package com.zyj.library.view.pulltorefresh;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.core.view.ViewCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.chad.library.adapter.base.BaseQuickAdapter;
+
+/**
+ * Created by lin on 2018/1/10.
+ */
+public class PullToRefreshRecyclerView extends PullToRefreshBase<RecyclerView> {
+
+    private RecyclerView rv;
+    /**
+     * 用于滑到底部自动加载的Footer
+     */
+    private LoadingLayout mLoadMoreFooterLayout;
+
+    /**
+     * 构造方法
+     *
+     * @param context context
+     */
+    public PullToRefreshRecyclerView(Context context) {
+        super(context);
+    }
+
+    /**
+     * 构造方法
+     *
+     * @param context context
+     * @param attrs   attrs
+     */
+    public PullToRefreshRecyclerView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    /**
+     * 构造方法
+     *
+     * @param context  context
+     * @param attrs    attrs
+     * @param defStyle defStyle
+     */
+    public PullToRefreshRecyclerView(Context context, AttributeSet attrs,
+                                     int defStyle) {
+        super(context, attrs, defStyle);
+        setPullLoadEnabled(false);
+    }
+
+    @Override
+    protected RecyclerView createRefreshableView(Context context, AttributeSet attrs) {
+        rv = new RecyclerView(context);
+        rv.setLayoutManager(new LinearLayoutManager(context));
+        rv.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                if (isScrollLoadEnabled() && hasMoreData()) {
+                    if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+                        if (isReadyForPullUp()) {
+                            startLoading();
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+            }
+        });
+        return rv;
+    }
+
+    /**
+     * 设置是否有更多数据的标志
+     *
+     * @param hasMoreData true表示还有更多的数据，false表示没有更多数据了
+     */
+    public void setHasMoreData(boolean hasMoreData) {
+        if (!hasMoreData) {
+            if (null != mLoadMoreFooterLayout) {
+                mLoadMoreFooterLayout.setState(ILoadingLayout.State.NO_MORE_DATA);
+            }
+
+            LoadingLayout footerLoadingLayout = getFooterLoadingLayout();
+            if (null != footerLoadingLayout) {
+                footerLoadingLayout.setState(ILoadingLayout.State.NO_MORE_DATA);
+            }
+        }
+    }
+
+    @Override
+    protected boolean isReadyForPullUp() {
+        return isLastItemVisible();
+    }
+
+    @Override
+    protected boolean isReadyForPullDown() {
+        return isFirstItemVisible();
+    }
+
+    @Override
+    protected void startLoading() {
+        super.startLoading();
+
+        if (null != mLoadMoreFooterLayout) {
+            mLoadMoreFooterLayout.setState(ILoadingLayout.State.REFRESHING);
+        }
+    }
+
+    @Override
+    public void onPullUpRefreshComplete() {
+        super.onPullUpRefreshComplete();
+
+        if (null != mLoadMoreFooterLayout) {
+            mLoadMoreFooterLayout.setState(ILoadingLayout.State.RESET);
+        }
+    }
+
+    @Override
+    public void setScrollLoadEnabled(boolean scrollLoadEnabled) {
+
+        if (isScrollLoadEnabled() == scrollLoadEnabled || rv.getAdapter() == null || !(rv.getAdapter() instanceof BaseQuickAdapter)) {
+            return;
+        }
+
+        super.setScrollLoadEnabled(scrollLoadEnabled);
+
+        if (scrollLoadEnabled) {
+            // 设置Footer
+            if (null == mLoadMoreFooterLayout) {
+                mLoadMoreFooterLayout = new FooterLoadingLayout(getContext());
+                ((BaseQuickAdapter) rv.getAdapter()).setFooterView(mLoadMoreFooterLayout);
+            }
+
+            mLoadMoreFooterLayout.show(true);
+        } else {
+            if (null != mLoadMoreFooterLayout) {
+                mLoadMoreFooterLayout.show(false);
+            }
+        }
+    }
+
+    @Override
+    public LoadingLayout getFooterLoadingLayout() {
+        if (isScrollLoadEnabled()) {
+            return mLoadMoreFooterLayout;
+        }
+
+        return super.getFooterLoadingLayout();
+    }
+
+    @Override
+    protected LoadingLayout createHeaderLoadingLayout(Context context,
+                                                      AttributeSet attrs) {
+        return new RotateLoadingLayout(context);
+    }
+
+    /**
+     * 表示是否还有更多数据
+     *
+     * @return true表示还有更多数据
+     */
+    private boolean hasMoreData() {
+        if ((null != mLoadMoreFooterLayout)
+                && (mLoadMoreFooterLayout.getState() == ILoadingLayout.State.NO_MORE_DATA)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * 判断第一个child是否完全显示出来
+     *
+     * @return true完全显示出来，否则false
+     */
+    private boolean isFirstItemVisible() {
+        return !ViewCompat.canScrollVertically(rv, -1);
+    }
+
+    /**
+     * 判断最后一个child是否完全显示出来
+     *
+     * @return true完全显示出来，否则false
+     */
+    private boolean isLastItemVisible() {
+        return !ViewCompat.canScrollVertically(rv, 1);
+    }
+}
---
+++ mylibrary/src/main/java/com/zyj/library/view/pulltorefresh/PullToRefreshScrollView.java
@@ -0,0 +1,105 @@
+package com.zyj.library.view.pulltorefresh;
+
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ScrollView;
+
+import com.zyj.library.R;
+
+
+/**
+ * 封装了ScrollView的下拉刷新
+ *
+ * @author Li Hong
+ * @since 2013-8-22
+ */
+public abstract class PullToRefreshScrollView extends PullToRefreshBase<ScrollView> {
+
+    View content;
+    ScrollView scrollView;
+	/**
+	 * 构造方法
+	 *
+	 * @param context
+	 *            context
+	 */
+	public PullToRefreshScrollView(Context context) {
+		// this(context, null);
+		super(context);
+	}
+
+	/**
+	 * 构造方法
+	 *
+	 * @param context
+	 *            context
+	 * @param attrs
+	 *            attrs
+	 */
+	public PullToRefreshScrollView(Context context, AttributeSet attrs) {
+		// this(context, attrs, 0);
+		super(context, attrs);
+	}
+
+	/**
+	 * 构造方法
+	 *
+	 * @param context
+	 *            context
+	 * @param attrs
+	 *            attrs
+	 * @param defStyle
+	 *            defStyle
+	 */
+	public PullToRefreshScrollView(Context context, AttributeSet attrs,
+                                   int defStyle) {
+		super(context, attrs, defStyle);
+	}
+
+	/**
+	 *
+	 */
+	@Override
+	protected ScrollView createRefreshableView(Context context,
+                                               AttributeSet attrs) {
+
+        content = createContentView();
+        scrollView = content.findViewById(R.id.root_view);
+		return scrollView;
+	}
+
+	protected abstract View createContentView();
+
+    public View getContent() {
+        return content;
+    }
+
+    /**
+	 */
+	@Override
+	protected boolean isReadyForPullDown() {
+		return mRefreshableView.getScrollY() == 0;
+	}
+
+	/**
+	 */
+	@Override
+	protected boolean isReadyForPullUp() {
+		View scrollViewChild = mRefreshableView.getChildAt(0);
+		if (null != scrollViewChild) {
+			return mRefreshableView.getScrollY() >= (scrollViewChild
+					.getHeight() - getHeight());
+		}
+
+		return false;
+	}
+
+	@Override
+	protected LoadingLayout createHeaderLoadingLayout(Context context,
+                                                      AttributeSet attrs) {
+		return new RotateLoadingLayout(context);
+	}
+
+}
